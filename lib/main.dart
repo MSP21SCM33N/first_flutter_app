2dd67d7e643f85f40514579c6ce2f68b0d59e280
Michael Neely <mneely1@hawk.iit.edu>
2019-05-20 17:26:24 -0500

    created a simple homepage with a white background and app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
-import 'package:flutter/material.dart'
-name = 'Max';
+import 'package:flutter/material.dart';
 
-main () {
+void main() => runApp(
+    MyApp()); // special function within the material package that actually loads the app
+// big arrow method of writing a function
 
-}
-class MyApp extends StatelessWidget{ // receives all the features from another class
-  build (context) {
-    return MaterialApp(); // Root widget
+class MyApp extends StatelessWidget {
+  // receives all the features from another class
+  @override // Tells flutter to override the build method
+  Widget build(BuildContext context) {
+    // Called the format shortcut from our keyboard to format our build code (shift, alt, f)
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Easy List'),
+        ),
+        body: Card(child: Column(children: <Widget>[
+          Image(),
+          Text('Food Paradise')
+        ],),),
+      ),
+    );
   }
 }
